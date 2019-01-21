9716b1fe40243348d4878dcd92767bfbf9556a64
TimHenkins <ethanrgroves4@gmail.com>
2019-01-21 09:55:57 -0800

    camera thig

--- Camera.py
+++ Camera.py
@@ -1,2 +1,15 @@
-Change
-install firefox to continue
+#!/usr/bin/env python3
+"""
+    This is a good foundation to build your robot code on
+"""
+
+import wpilib
+
+
+class MyRobot(wpilib.IterativeRobot):
+    def robotInit(self):
+        wpilib.CameraServer.launch()
+
+
+if __name__ == "__main__":
+wpilib.run(MyRobot)
